cda250f0fdbea283473bce63c3cbff4da6abeedd
Withered Flowers <withered-flowers@github.com>
2020-05-07 16:49:26 +0700

    Initial commit for backup

---
+++ src/main/kotlin/com/witheredflowers/mpdk/tugas01/programpertamakotlin/latihan02/Tugas.kt
@@ -0,0 +1,10 @@
+package com.witheredflowers.mpdk.tugas01.programpertamakotlin.latihan02
+
+fun main() {
+//    println("Kotlin,\nis Awesome!")
+    println(
+        """"Kotlin,
+            |is Awesome!
+        """.trimMargin()
+    )
+}
